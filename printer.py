7726af6dc3599503d1abb6c5947356bef791da91
FaNat1k8512 <rusaznal777@gmail.com>
2025-05-07 19:30:45 +0500

    Printer 2.1 МОСШТАБНОЕ ОБНОВЛЕНИЕ (члены не добавим)

--- printer.py
+++ printer.py
@@ -1,3 +1,8 @@
+# Функция:
+def reverseName(word): 
+    result = word[::-1]
+    return result 
+
 # Эта программа принтер 2.0!
 
 print('Вас приветствует Принтер 2.0, введите имя.')
@@ -10,3 +15,14 @@ if name == "Ellis":
 
 if name == "Fanat":
     print('Здравствуйте великий создатель')
+
+if name == "DLTima":
+    print('Вам вход запрещён. Уходите. Это приказ. пожалуйста...')
+
+# цикл
+# for i in range(10):
+#     print(name)
+
+# Тест Функции
+print(reverseName(name))
+
