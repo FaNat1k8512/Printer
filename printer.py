aac0441837499c2312fcc4dd3899ff2a50b77b19
FaNat1k8512 <rusaznal777@gmail.com>
2025-05-07 20:00:07 +0500

    Printer 2.1.1 + Запись.

--- printer.py
+++ printer.py
@@ -1,7 +1,20 @@
+# Импорты
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__))) # Устанавливаем путь выполнения кода
+
 # Функция:
 def reverseName(word): 
     result = word[::-1]
-    return result 
+    return result
+
+def create_directory(directory_path):
+    try:
+        # Создаем директорию, если она не существует
+        os.makedirs(directory_path, exist_ok=True)
+        print(f"Директория '{directory_path}' успешно создана.")
+    except Exception as e:
+        print(f"\033[31mОшибка при создании директории: {e}") 
 
 # Эта программа принтер 2.0!
 
@@ -13,16 +26,22 @@ print('Здравствуйте господин', name)
 if name == "Ellis":
     print('Здравствуйте хозяин')
 
-if name == "Fanat":
+elif name == "Fanat":
     print('Здравствуйте великий создатель')
 
-if name == "DLTima":
+elif name == "DLTima":
     print('Вам вход запрещён. Уходите. Это приказ. пожалуйста...')
 
+elif name == "член":
+    print('Я же сказал, члены не добавим! (навреное)')
+
+else:
+    print(f'Добро пожаловать, {name}.') 
+
+ 
 # цикл
 # for i in range(10):
 #     print(name)
 
 # Тест Функции
 print(reverseName(name))
-
